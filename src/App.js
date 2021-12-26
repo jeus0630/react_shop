6f42c186c3ea105f7617446f9cf7017e6609c543
jewoo <jeus0630@gmail.com>
2021-12-26 12:47:55 -0500

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
+import Product from './data';
+import {useState} from "react";
+import CompDetail from "./Detail";
+import { Link, Route, Switch, Routes} from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
+    let [product, productChange] = useState(Product);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                        <Nav.Link as={Link} to={"/detail"}>Detail</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        <Routes>
+            <Route path="/" element={<CompMain product={product} productChange={productChange}></CompMain>}></Route>
+            <Route path="/detail/:id" element={<CompDetail product={product}></CompDetail>}></Route>
+            <Route path="/:id" element={<div>아무거나 보여주셈</div>}></Route>
+        </Routes>
+
     </div>
   );
+}
+
+function CompMain({product, productChange}){
+    return (
+        <div className="container">
+            <div className="row">
+                {
+                    product.map((el,idx)=> <CompProduct productData={product[idx]} key={idx}></CompProduct>)
+                }
+            </div>
+            <button className="btn btn-primary" onClick={()=>{
+                axios.get('https://codingapple1.github.io/shop/data2.json')
+                    .then(({data})=>{
+                        productChange([...product, ...data]);
+                })
+                    .catch(()=>{});
+            }}>더보기</button>
+        </div>
+    )
+}
+
+function CompProduct({productData}){
+    return(
+        <div className="col-md-4">
+            <img src={productData.src} alt="" width="100%"/>
+            <h4>{productData.title}</h4>
+            <p>{productData.content} & {productData.price}</p>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Detail.js
@@ -0,0 +1,56 @@
+import {useNavigate, useParams} from 'react-router-dom';
+import styled from 'styled-components';
+import './comp-detail.css';
+import {useEffect, useState} from "react";
+
+const Box = styled.div`
+  padding : 20px;
+`;
+
+const Title = styled.h4`
+  font-size : 25px;  
+`;
+
+export default function({product}){
+
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    let [onShowJumbotron, onShowJumbotronChange] = useState(true);
+    let [inputValue, inputValueChange] = useState('');
+
+    useEffect(()=>{
+
+        return ()=>{
+
+        }
+    },[]);
+
+    return (
+        <div className="container comp-detail">
+            <Box>
+                <Title>상세페이지</Title>
+            </Box>
+
+            <input type="text" onChange={(e)=>{inputValueChange(e.target.value)}}/>
+            
+            {
+             onShowJumbotron? <div className="jumbotron">
+                 SALE SALE SALE
+             </div> : null
+            }
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{product[id].title}</h4>
+                    <p>{product[id].content}</p>
+                    <p>{product[id].price}</p>
+                    <button className="btn btn-danger">주문하기</button>
+                    <button className="btn btn-danger" onClick={()=>{navigate(-1)}}>뒤로가기</button>
+                </div>
+            </div>
+        </div>
+    )
+}
